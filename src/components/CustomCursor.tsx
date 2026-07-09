de60960d6a016f619b3df1ba4649cc4001584abf
jaidevms <ms.jaidev.jd17@gmail.com>
2026-07-09 14:29:41 +0530

    cursor color change

--- src/components/CustomCursor.tsx
+++ src/components/CustomCursor.tsx
@@ -26,7 +26,7 @@ export function CustomCursor() {
       const target = (e.target as HTMLElement)?.closest("a, button");
       gsap.to(dot, {
         scale: target ? 4 : 1,
-        backgroundColor: target ? "#fed400" : "#0a0a0a",
+        backgroundColor: "#D7B4F3",
         duration: 0.2,
       });
     };
@@ -45,7 +45,7 @@ export function CustomCursor() {
     <div
       ref={dotRef}
       aria-hidden="true"
-      className="pointer-events-none fixed left-0 top-0 z-[100] hidden h-2 w-2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-brand-black [@media(hover:hover)_and_(pointer:fine)]:block"
+      className="pointer-events-none fixed left-0 top-0 z-[100] hidden h-2 w-2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#D7B4F3] [@media(hover:hover)_and_(pointer:fine)]:block"
     />
   );
 }
